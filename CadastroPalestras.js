b5acf1e9df40866db2d8a3dafdf28275d4185ea4
Caue Oliveira <cogumelogia@gmail.com>
2021-10-14 10:23:57 -0300

    código com sistema até o fim, com modificações feitas diretamente na programação que diferem do fluxograma. Cadastro de palestrante ainda faltando

--- CadastroPalestras.js
+++ CadastroPalestras.js
@@ -27,20 +27,21 @@ if (usuarioFuncao == tipoUsuario[0]){
 }
 /// No fluxograma eu estipulei apenas o acesso de "participantes", mas seria interessante colocar acesso de cadastro de "palestrantes" no sistema também.
 /// Para isso, eu queria aninhar as linhas seguintes para dar dois caminhos na sequência de inserção de dados.
-/// Um caminho para o "palestrante" e outro para o "participante"
+/// Um caminho para o "palestrante" e outro para o "participante".
 /// Continuarei na sequência apenas para o "participante" até encontrar uma solução para isso.
 
 let dataAtual = new Date ("10/13/2021");
 let dataEscolhida = new Date ("11/04/2021");
 
 if (dataEscolhida > dataAtual){
-    console.log (`${dataEscolhida.toLocaleDateString("pt-BR")}`); 
+    console.log (`${dataEscolhida.toLocaleDateString("en-US")}`); 
     console.log ("Data escolhida! Veja abaixo a lista de palestras do dia escolhido:")
         }else{
             console.log ("Data inválida. Data inserida anterior à data atual. Escolha uma nova data");
 }
-/// Implementei função nativa do javascript para arrumar a data em modelo DD/MM/AAAA.
-/// Porém ainda não achei uma solução para a inserção de dados ser através do modelo de data brasileiro.
+/// Implementei função nativa do javascript para arrumar a data em modelo MM/DD/AAAA.
+/// Porém ainda não achei uma solução para a inserção de dados ser através do modelo de data brasileiro 'new Date'.
+/// Só consegui achar como colocar data em modelo pt-BR (DD/MM/AAAA) usando o toLocaleDateString.
 
 let listaPalestras = ["Como vender melhor suas ideias", "10 passos para ser mais produtivo", "A importância de saber contar uma boa história"];
 let palestrasCadastradas = [];
@@ -55,3 +56,34 @@ let palestrasCadastradas = [];
         }
         palestrasCadastradas++
     }
+
+console.log ("- Selecione a palestra que você quer participar -");
+
+let numCadastradosPalestra1 = 100;
+let numCadastradosPalestra2 = 100;
+let numCadastradosPalestra3 = 80;
+
+let palestraEscolhida = numCadastradosPalestra3
+
+if (palestraEscolhida < 100){
+    console.log ("Você garantiu sua vaga! Termine a inscrição adicionando seu nome completo, e-Mail e telefone para contato.")
+    }else{
+        console.log ("Limite de participantes excedido para a palestra escolhida. Escolha outra palestra.")
+}
+
+let dadosUsuario = ["Nome:", "João", "Sobrenome:", "Silva", "e-Mail:", "js2000@email.com.br", "Telefone:", "(99) 55555-5555"]
+let listaDados = dadosUsuario.length
+let dadosUsuario1 = dadosUsuario.join (' ') ///variável para tirar vírgula de uma array
+
+if (listaDados < 8){
+    console.log ("Dados incompletos. Preencha todos os campos com seus dados")
+    }else{
+        console.log (`${dadosUsuario1}`)
+        console.log ("Participante cadastrado com sucesso! Marque a data da palestra escolhida em sua agenda.")
+        console.log ("30 minutos antes da palestra, iremos enviar um alerta em seu e-Mail cadastrado.")
+}
+
+///Revendo meu fluxograma, acabei invertando algumas condições de 'if/else' para ficar melhor. 
+///Algumas perguntas como "número de participantes maior que 100", por exemplo, optei no código usar como condição verdadeira o contrário.
+///Ainda quero colocar no sistema a possibilidade de continuar o cadastro como palestrante.
+///Mas para continuarmos como o MVP (Minimum Valuable Product), deixarei o código como está e fazer modificações posteriores.
